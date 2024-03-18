96938864e9d684e545c13a16475e7bb48791453f
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-03-18 03:29:59 -0700

    Add files via upload

---
+++ Basic/java2.java
@@ -0,0 +1,7 @@
+public class java2 {
+
+    public static void main(String[] args) {
+        String name = "Zahid";
+        System.out.println(name.length());
+    }
+}
---
+++ Basic/java3.java
@@ -0,0 +1,8 @@
+public class java3 {
+    public static void main(String[] args) {
+        String name = "Zahid";
+        String cast = "Rahimoon";
+        String fullname = name + " " + cast;
+        System.out.println(fullname);
+    }
+}
