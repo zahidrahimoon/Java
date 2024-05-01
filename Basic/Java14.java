beea3839832766af6e29332774c79db1c2649f7a
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-05-01 16:29:03 +0500

    Practice

---
+++ Basic/Java14.java
@@ -0,0 +1,12 @@
+public class Java14 {
+    public static void main(String[] args) {
+        int n = 4; 
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
